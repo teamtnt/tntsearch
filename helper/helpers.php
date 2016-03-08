60a7ff1b8a5189193f5fe6ba6e21f2741de55f31
Nenad Ticaric <nticaric@gmail.com>
2016-03-08 11:30:35 +0100

    check if function exists

--- helper/helpers.php
+++ helper/helpers.php
@@ -1,6 +1,9 @@
 <?php
 
-function dd() {
-    array_map(function($x) { var_dump($x); }, func_get_args());
-    die;
+if ( ! function_exists('dd')) {
+    
+    function dd() {
+        array_map(function($x) { var_dump($x); }, func_get_args());
+        die;
+    }
 }
