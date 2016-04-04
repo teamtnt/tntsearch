d273d97f8a6d6bb7532ed58ccec45e61e6021743
Nenad Ticaric <nticaric@gmail.com>
2016-04-04 20:05:32 +0200

    phpunit test for german stemmer

---
+++ tests/stemmer/GermanStemmerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use TeamTNT\TNTSearch\Stemmer\GermanStemmer;
+
+class GermanStemmerTestTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testStem()
+    {
+        $stemmer = new GermanStemmer;
+        $this->assertEquals("vergnug", $stemmer->stem("vergnüglich"));
+    }
+}
