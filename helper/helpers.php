358edf0b58e853a7249efda2ef1a1ddd212cfa25
Nenad Ticaric <nticaric@gmail.com>
2016-04-27 12:22:55 +0200

    code cleanup

--- helper/helpers.php
+++ helper/helpers.php
@@ -1,15 +1,17 @@
 <?php
 
-if ( ! function_exists('dd')) {
-    
-    function dd() {
-        array_map(function($x) { var_dump($x); }, func_get_args());
+if (!function_exists('dd')) {
+
+    function dd()
+    {
+        array_map(function ($x) {var_dump($x);}, func_get_args());
         die;
     }
 }
 
-if ( ! function_exists('stringEndsWith')) {
-    function stringEndsWith($haystack, $needle) {
+if (!function_exists('stringEndsWith')) {
+    function stringEndsWith($haystack, $needle)
+    {
         // search forward starting from end minus needle length characters
         return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== false);
     }
--- src/support/Hihglighter.php
+++ src/support/Hihglighter.php
@@ -3,27 +3,27 @@
 namespace TeamTNT\TNTSearch\Support;
 
 class Hihglighter
-{   
+{
     protected $options = [
         'simple'        => false,
-        'wholeWord'     => true, 
-        'caseSensitive' => false, 
-        'stripLinks'    => false
+        'wholeWord'     => true,
+        'caseSensitive' => false,
+        'stripLinks'    => false,
     ];
 
     public function highlight($text, $needle, $tag = 'em', $options = [])
     {
         $this->options = array_merge($this->options, $options);
 
-        $highlight = '<'.$tag.'>\1</'.$tag.'>';
-        $needle = preg_split('/\PL+/u', $needle, -1, PREG_SPLIT_NO_EMPTY);
+        $highlight = '<' . $tag . '>\1</' . $tag . '>';
+        $needle    = preg_split('/\PL+/u', $needle, -1, PREG_SPLIT_NO_EMPTY);
 
         // Select pattern to use
         if ($this->options['simple']) {
-            $pattern = '#(%s)#';
+            $pattern    = '#(%s)#';
             $sl_pattern = '#(%s)#';
         } else {
-            $pattern = '#(?!<.*?)(%s)(?![^<>]*?>)#';
+            $pattern    = '#(?!<.*?)(%s)(?![^<>]*?>)#';
             $sl_pattern = '#<a\s(?:.*?)>(%s)</a>#';
         }
 
@@ -45,97 +45,103 @@ class Hihglighter
             // Strip links
             if ($this->options['stripLinks']) {
                 $sl_regex = sprintf($sl_pattern, $needle_s);
-                $text = preg_replace($sl_regex, '\1', $text);
+                $text     = preg_replace($sl_regex, '\1', $text);
             }
 
             $regex = sprintf($pattern, $needle_s);
-            $text = preg_replace($regex, $highlight, $text);
+            $text  = preg_replace($regex, $highlight, $text);
         }
 
         return $text;
     }
 
     // find the locations of each of the words
-    // Nothing exciting here. The array_unique is required 
+    // Nothing exciting here. The array_unique is required
     // unless you decide to make the words unique before passing in
-    public function _extractLocations($words, $fulltext) {
+    public function _extractLocations($words, $fulltext)
+    {
         $locations = array();
-        foreach($words as $word) {
+        foreach ($words as $word) {
             $wordlen = strlen($word);
-            $loc = stripos($fulltext, $word);
-            while($loc !== FALSE) {
+            $loc     = stripos($fulltext, $word);
+            while ($loc !== false) {
                 $locations[] = $loc;
-                $loc = stripos($fulltext, $word, $loc + $wordlen);
+                $loc         = stripos($fulltext, $word, $loc + $wordlen);
             }
         }
         $locations = array_unique($locations);
         sort($locations);
-        
+
         return $locations;
     }
 
     // Work out which is the most relevant portion to display
-    // This is done by looping over each match and finding the smallest distance between two found 
-    // strings. The idea being that the closer the terms are the better match the snippet would be. 
-    // When checking for matches we only change the location if there is a better match. 
-    // The only exception is where we have only two matches in which case we just take the 
+    // This is done by looping over each match and finding the smallest distance between two found
+    // strings. The idea being that the closer the terms are the better match the snippet would be.
+    // When checking for matches we only change the location if there is a better match.
+    // The only exception is where we have only two matches in which case we just take the
     // first as will be equally distant.
-    public function _determineSnipLocation($locations, $prevcount) {
-        if(!isset($locations[0])) return -1;
+    public function _determineSnipLocation($locations, $prevcount)
+    {
+        if (!isset($locations[0])) {
+            return -1;
+        }
+
         // If we only have 1 match we dont actually do the for loop so set to the first
-        $startpos = $locations[0];  
-        $loccount = count($locations);
-        $smallestdiff = PHP_INT_MAX;    
+        $startpos     = $locations[0];
+        $loccount     = count($locations);
+        $smallestdiff = PHP_INT_MAX;
         // If we only have 2 skip as its probably equally relevant
-        if(count($locations) > 2) {
+        if (count($locations) > 2) {
             // skip the first as we check 1 behind
-            for($i=1; $i < $loccount; $i++) { 
-                if($i == $loccount-1) { // at the end
-                    $diff = $locations[$i] - $locations[$i-1];
+            for ($i = 1; $i < $loccount; $i++) {
+                if ($i == $loccount - 1) {
+                    // at the end
+                    $diff = $locations[$i] - $locations[$i - 1];
+                } else {
+                    $diff = $locations[$i + 1] - $locations[$i];
                 }
-                else {
-                    $diff = $locations[$i+1] - $locations[$i];
-                }
-                
-                if($smallestdiff > $diff) {
+
+                if ($smallestdiff > $diff) {
                     $smallestdiff = $diff;
-                    $startpos = $locations[$i];
+                    $startpos     = $locations[$i];
                 }
             }
         }
-            
+
         $startpos = $startpos > $prevcount ? $startpos - $prevcount : 0;
         return $startpos;
     }
 
     // 1/6 ratio on prevcount tends to work pretty well and puts the terms
     // in the middle of the extract
-    public function extractRelevant($words, $fulltext, $rellength=300, $prevcount=50, $indicator='...') {
-        $words = preg_split('/\PL+/u', $words, -1, PREG_SPLIT_NO_EMPTY);
+    public function extractRelevant($words, $fulltext, $rellength = 300, $prevcount = 50, $indicator = '...')
+    {
+        $words      = preg_split('/\PL+/u', $words, -1, PREG_SPLIT_NO_EMPTY);
         $textlength = strlen($fulltext);
-        if($textlength <= $rellength) {
+        if ($textlength <= $rellength) {
             return $fulltext;
         }
-        
+
         $locations = $this->_extractLocations($words, $fulltext);
-        $startpos  = $this->_determineSnipLocation($locations,$prevcount);
+        $startpos  = $this->_determineSnipLocation($locations, $prevcount);
         // if we are going to snip too much...
-        if($textlength-$startpos < $rellength) {
-            $startpos = $startpos - ($textlength-$startpos)/2;
+        if ($textlength - $startpos < $rellength) {
+            $startpos = $startpos - ($textlength - $startpos) / 2;
         }
-        
+
         $reltext = substr($fulltext, $startpos, $rellength);
-        
+
         // check to ensure we dont snip the last word if thats the match
-        if( $startpos + $rellength < $textlength) {
-            $reltext = substr($reltext, 0, strrpos($reltext, " ")).$indicator; // remove last word
+        if ($startpos + $rellength < $textlength) {
+            $reltext = substr($reltext, 0, strrpos($reltext, " ")) . $indicator; // remove last word
         }
-        
+
         // If we trimmed from the front add ...
-        if($startpos != 0) {
-            $reltext = $indicator.substr($reltext, strpos($reltext, " ") + 1); // remove first word
+        if ($startpos != 0) {
+            $reltext = $indicator . substr($reltext, strpos($reltext, " ") + 1); // remove first word
         }
-        
+
         return $reltext;
     }
 }
