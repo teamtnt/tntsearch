a15360db8aeb1ffc9f057b7b5b6a8b3fda50eb86
Lucas Padilha <padilhaght@gmail.com>
2017-09-29 01:55:46 -0300

    Portuguese Stemmer Test

--- tests/stemmer/PortugueseStemmerTest.php
+++ tests/stemmer/PortugueseStemmerTest.php
@@ -1,12 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Lucas
- * Date: 29/09/2017
- * Time: 01:50
- */
 
-class PortugueseStemmerTest
+use TeamTNT\TNTSearch\Stemmer\PortugueseStemmer;
+
+class PortugueseStemmerTestTest extends PHPUnit_Framework_TestCase
 {
 
+    public function testStem()
+    {
+        $stemmer = new PortugueseStemmer;
+        $this->assertEquals("gost", $stemmer->stem("gostou"));
+        $this->assertEquals("gost", $stemmer->stem("gosto"));
+        $this->assertEquals("gost", $stemmer->stem("gostaram"));
+    }
 }
