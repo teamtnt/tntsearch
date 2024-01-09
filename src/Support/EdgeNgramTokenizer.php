c6658fec1afe23dc9b376e6e82959544a2731f0d
Hamster.Xu <449211678@qq.com>
2024-01-09 12:07:32 +0800

    Update EdgeNgramTokenizer.php
    fixed EdgeNgramTokenizer split word count

--- src/Support/EdgeNgramTokenizer.php
+++ src/Support/EdgeNgramTokenizer.php
@@ -13,7 +13,7 @@ class EdgeNgramTokenizer extends AbstractTokenizer implements TokenizerInterface
         $splits = preg_split($this->getPattern(), $text, -1, PREG_SPLIT_NO_EMPTY);
 
         foreach ($splits as $split) {
-            for ($i = 2; $i <= strlen($split); $i++) {
+            for ($i = 2; $i <= mb_strlen($split); $i++) {
                 $ngrams[] = mb_substr($split, 0, $i);
             }
         }
